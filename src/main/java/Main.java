ab745387a544b5f7dc8d213ae54887364c8a31f8
joseph thepa <mmthejhb025@student.wethinkcode.co.za>
2026-05-10 12:55:07 +0200

    working

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,9 +2,11 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-         Scanner scanner = new Scanner(System.in);
-         System.out.print("$ ");
-         String command =scanner.next();
-         System.out.println(command+": command not found");
+        while(true) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("$ ");
+            String command = scanner.next();
+            System.out.println(command + ": command not found");
+        }
     }
 }
