5e5709d6000eb76978088732c8e973056beb8209
joseph thepa <mmthejhb025@student.wethinkcode.co.za>
2026-05-10 13:50:28 +0200

    working

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Objects;
 import java.util.Scanner;
 
@@ -7,11 +8,16 @@ public class Main {
             Scanner scanner = new Scanner(System.in);
             System.out.print("$ ");
             String command = scanner.nextLine();
+            String[] types = {"type","echo","exit"};
             if(Objects.equals(command.toLowerCase(), "exit")){
                 return;
             } else if (command.startsWith("echo ")) {
                 System.out.println(command.substring(5));
-            }else{
+            } else if (command.startsWith("type ")) {
+                if(command.substring(5).contains(Arrays.toString(types))){
+                    System.out.println(command.substring(5)+ ": is a shell builtin");
+                }
+            } else{
                 System.out.println(command+ ": command not found");
             }
 
