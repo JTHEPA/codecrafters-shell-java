5059d4fd0a2e98585d152a5b0a2c5f7ca9331bf5
joseph thepa <mmthejhb025@student.wethinkcode.co.za>
2026-05-10 13:36:07 +0200

    working

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,11 +6,11 @@ public class Main {
         while(true){
             Scanner scanner = new Scanner(System.in);
             System.out.print("$ ");
-            String command = scanner.next();
+            String command = scanner.nextLine();
             if(Objects.equals(command.toLowerCase(), "exit")){
                 return;
             } else if (command.startsWith("echo ")) {
-                System.out.println(command.substring(5));
+                System.out.println(command.substring(4));
             }else{
                 System.out.println(command+ ": command not found");
             }
